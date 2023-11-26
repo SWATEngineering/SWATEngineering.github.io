efebc3ea149c9213656c14930447e936af417edb
simocare <simone.caregnato@studenti.unipd.it>
2023-11-26 23:19:50 +0100

    aggiunti al sito i documenti 2_RTB

--- script.js
+++ script.js
@@ -7,24 +7,50 @@ const Titles = [];
 const candidaturaTitle = document.querySelector('#documenti #candidatura');
 const candidaturaSection = document.querySelector('#documenti #candidatura-section');
 
-const verbaliEsterniTitle = document.querySelector('#documenti #verbaliEsterni');
-const verbaliEsterniSection = document.querySelector('#documenti #verbaliEsterni-section');
+const verbaliEsterniTitleC = document.querySelector('#documenti #verbaliEsterni.cand');
+const verbaliEsterniSectionC = document.querySelector('#documenti #verbaliEsterni-section.cand');
 
-const verbaliInterniTitle = document.querySelector('#documenti #verbaliInterni');
-const verbaliInterniSection = document.querySelector('#documenti #verbaliInterni-section');
+const verbaliInterniTitleC = document.querySelector('#documenti #verbaliInterni.cand');
+const verbaliInterniSectionC = document.querySelector('#documenti #verbaliInterni-section.cand');
 
-const ddbTitle = document.querySelector('#documenti #ddb');
-const ddbSection = document.querySelector('#documenti #ddb-section');
+const rtbTitle = document.querySelector('#documenti #RTB');
+const rtbSection = document.querySelector('#documenti #RTB-section');
+
+const verbaliEsterniTitleRTB = document.querySelector('#documenti #verbaliEsterni.rtb');
+const verbaliEsterniSectionRTB = document.querySelector('#documenti #verbaliEsterni-section.rtb');
+const verbaliInterniTitleRTB = document.querySelector('#documenti #verbaliInterni.rtb');
+const verbaliInterniSectionRTB = document.querySelector('#documenti #verbaliInterni-section.rtb');
+
+const adrTitle = document.querySelector('#documenti #AdR');
+const adRSection = document.querySelector('#documenti #AdR-section');
+const glossarioTitle = document.querySelector('#documenti #glossario');
+const glossarioSection = document.querySelector('#documenti #glossario-section');
+const ndpTitle = document.querySelector('#documenti #NdP');
+const ndpSection = document.querySelector('#documenti #NdP-section');
+const pdpTitle = document.querySelector('#documenti #PdP');
+const pdpSection = document.querySelector('#documenti #PdP-section');
 
 Titles.push(candidaturaTitle);
-Titles.push(verbaliEsterniTitle);
-Titles.push(verbaliInterniTitle);
-Titles.push(ddbTitle);
+Titles.push(verbaliEsterniTitleC);
+Titles.push(verbaliInterniTitleC);
+Titles.push(rtbTitle);
+Titles.push(verbaliEsterniTitleRTB);
+Titles.push(verbaliInterniTitleRTB);
+Titles.push(adrTitle);
+Titles.push(glossarioTitle);
+Titles.push(ndpTitle);
+Titles.push(pdpTitle);
 
 Links.push(candidaturaSection);
-Links.push(verbaliEsterniSection);
-Links.push(verbaliInterniSection);
-Links.push(ddbSection);
+Links.push(verbaliEsterniSectionC);
+Links.push(verbaliInterniSectionC);
+Links.push(rtbSection);
+Links.push(verbaliEsterniSectionRTB);
+Links.push(verbaliInterniSectionRTB);
+Links.push(adRSection);
+Links.push(glossarioSection);
+Links.push(ndpSection);
+Links.push(pdpSection);
 
 //per ogni link in Links, imposto il display a none
 Links.forEach((link) => {

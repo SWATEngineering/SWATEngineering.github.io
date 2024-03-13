a98206706e17d368e985ea6db0e35d11345c31a4
kara <ssctonioloriccardo@gmail.com>
2024-03-13 11:43:50 +0100

    New link style on sizes

--- automate-cards.js
+++ automate-cards.js
@@ -43,7 +43,7 @@ function from_item_to_card(item, depth) {
     let closing_tag =
         item.isDir == 1
             ? `</h${depth}>`
-            : `<span>(download ${item.size})</span></p>`;
+            : ` <span id="download">(download ${item.size})</span></p>`;
     let card = `
             <a href="${item.url}" target="_blank">${opening_tag}${icon} ${item.name}${closing_tag}</a>
         `;
